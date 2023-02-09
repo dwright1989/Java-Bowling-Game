baba7935f798dd9649a07c813a02ed70c73db75e
dwright1989 <debbiewright@live.co.uk>
2023-02-09 08:53:41 +0000

    Tests passing for strike in first frame

--- src/main/java/com/techreturners/BowlingGame.java
+++ src/main/java/com/techreturners/BowlingGame.java
@@ -5,7 +5,10 @@ public class BowlingGame {
     private int score = 0;
     public int getGameScore(int[][] rolls){
         for (int i=0; i<10; i++) {
-            if((rolls[i][0]+rolls[i][1])==10){
+            if(rolls[i][0]==10){
+                score+= 10+rolls[i+1][0] + rolls[i+1][1];
+            }
+            else if((rolls[i][0]+rolls[i][1])==10){
                 score+=rolls[i][0] + rolls[i][1] + rolls[i+1][0];
             }else{
                 score += rolls[i][0] + rolls[i][1];
--- src/test/java/com/techreturners/BowlingGameTest.java
+++ src/test/java/com/techreturners/BowlingGameTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BowlingGameTest {
 
+
+
     @Test
     public void getScoreForZeros(){
         int[][] rolls = {{0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}};
@@ -48,6 +50,12 @@ public class BowlingGameTest {
         int[][] rolls = {{5,1}, {1,7}, {2,4}, {4,4}, {1,8}, {8,1}, {8,1}, {0,1}, {7,3}, {3,7}, {6}};
         BowlingGame game = new BowlingGame();
         assertEquals(85, game.getGameScore(rolls));
+    }
+    @Test
+    public void getScoreForRollsWithStrikeAtStart(){
+        int[][] rolls = {{10,0}, {1,7}, {2,4}, {4,4}, {1,8}, {8,1}, {8,1}, {0,1}, {7,1}, {3,1}};
+        BowlingGame game = new BowlingGame();
+        assertEquals(80, game.getGameScore(rolls));
     }
 
 }
