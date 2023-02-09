f862f2f114972a81eea9c4146aed7279eed4872b
dwright1989 <debbiewright@live.co.uk>
2023-02-09 08:30:29 +0000

    Test for all ones passing

--- src/main/java/com/techreturners/BowlingGame.java
+++ src/main/java/com/techreturners/BowlingGame.java
@@ -1,4 +1,12 @@
 package com.techreturners;
 
 public class BowlingGame {
+
+    private int score = 0;
+    public int getGameScore(int[][] rolls){
+        for (int[] roll : rolls) {
+            score += roll[0] + roll[1];
+        }
+        return score;
+    }
 }
--- src/test/java/com/techreturners/BowlingGameTest.java
+++ src/test/java/com/techreturners/BowlingGameTest.java
@@ -1,4 +1,23 @@
 package com.techreturners;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BowlingGameTest {
+
+    @Test
+    public void getScoreForZeros(){
+        int[][] rolls = {{0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}};
+        BowlingGame game = new BowlingGame();
+        assertEquals(0, game.getGameScore(rolls));
+
+    }
+
+    @Test
+    public void getScoreForOnes(){
+        int[][] rolls = {{1,1}, {1,1}, {1,1}, {1,1}, {1,1}, {1,1}, {1,1}, {1,1}, {1,1}, {1,1}};
+        BowlingGame game = new BowlingGame();
+        assertEquals(20, game.getGameScore(rolls));
+
+    }
+
 }
